fe2584806eda17b3b1fad9b3b67cb400f6823b73
Alex Ford <>
2019-08-04 13:52:19 -0700

    Fixed issue where CSS did not load on home page when deployed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import SEO from "../components/seo"
-import Login from "../components/login"
+import SEO from '../components/seo';
+import Login from '../components/login';
+import '../components/layout.css';
 
 const IndexPage = () => (
   <>
@@ -10,8 +11,7 @@ const IndexPage = () => (
     <div className="flex items-center justify-center h-screen">
       <Login />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </>
-)
+);
 
-export default IndexPage
+export default IndexPage;
